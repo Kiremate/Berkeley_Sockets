166317dad62d1ef3a192cd5d594a36412dc36f24
informaticaesne <informatica@esne.es>
2021-10-18 17:47:28 +0200

    Added a new project for engine dev v.2019

---
+++ headers/macros.h
@@ -0,0 +1,7 @@
+//
+#pragma once 
+
+#if __cplusplus < 201103L
+    #define ENGINE_CPP_98
+#elif 
+    #define ENGINE_CPP_
---
+++ headers/types.h
@@ -0,0 +1,8 @@
+//
+
+#pragma once
+#include "macros.h"
+
+if !defined(ENGINE_CPP_20)
+    typedef unsigned char char8_t;
+#endif
