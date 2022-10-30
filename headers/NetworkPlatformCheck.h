079e3d2f168f3dd6fafb0964f42f4cac6375e96b
notbloodmask <erik.mateos@cryptoavatars.io>
2022-10-30 20:02:59 +0100

    Created macros file to avoid name collision between WinSock.h located in Windows.h and WinSock2.h
    Created NetworkPlatformCheck to support POSIX platforms socket dev.

---
+++ headers/NetworkPlatformCheck.h
@@ -0,0 +1,38 @@
+//*THIS FILE IS USED TO INCLUDE NETWORK DEPENDENCIES DEPENDING ON THE OS*/
+#pragma once
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	//define something for Windows (32-bit and 64-bit, this part is common)
+	#include <WinSock2.h>
+	#include <WS2tcpip.h>
+	#ifdef _WIN64
+   //define something for Windows (64-bit only)
+	#else
+   //define something for Windows (32-bit only)
+	#endif
+#elif __APPLE__
+#include <TargetConditionals.h>
+	#if TARGET_IPHONE_SIMULATOR
+	// iOS, tvOS, or watchOS Simulator
+	#elif TARGET_OS_MACCATALYST
+	// Mac's Catalyst (ports iOS API into Mac, like UIKit).
+	#elif TARGET_OS_IPHONE
+	// iOS, tvOS, or watchOS device
+	#elif TARGET_OS_MAC
+	// Other kinds of Apple platforms
+	#else
+	#   error "Unknown Apple platform"
+	#endif
+#elif __ANDROID__
+// Below __linux__ check should be enough to handle Android,
+// but something may be unique to Android.
+	#elif __linux__
+	// linux
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#elif __unix__ // all unices not caught above
+	// Unix
+	#elif defined(_POSIX_VERSION)
+	// POSIX
+	#else
+	#   error "Unknown compiler"
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../headers/macros.h"
+#include "../headers/NetworkPlatformCheck.h"
+
+using namespace std;
+
+int main() {
+	
+	SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	//SOCKET tcpSocket = socket(AF_INET6, SOCK_STREAM, 0);
+	
+
+
+	
+	
+	return 0;
+}
