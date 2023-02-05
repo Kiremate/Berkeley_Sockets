150a540603043786007aaaa39820b7a7fd780f4d
Bloodmask <kiremate@hotmail.com>
2023-02-05 13:36:18 +0100

    Added some comments to SocketAddressFactory

--- headers/SocketAddressFactory.h
+++ headers/SocketAddressFactory.h
@@ -11,40 +11,52 @@ namespace BloodSockets{
 	// @license MIT
 	class SocketAddressFactory {
 	public:
+		// CreateIPv4FromString method
+		// This method creates a SocketAddress object using an IPv4 address and port number.
+		// The address and port are passed as a string in the format "host:port".
+		// @param inString: The string containing the address and port number.
+		// @return The created SocketAddress object as a shared pointer.
+		// @return nullptr if the address and port information is invalid.
 		static SocketAddressPtr CreateIPv4FromString(const std::string & inString) {
+			// Find the position of the colon character in the string
 			auto pos = inString.find_last_of(':');
 			std::string host, service;
 			if (pos != std::string::npos) {
+				// If the colon is found, the string is split into host and service strings
 				host = inString.substr(0, pos);
 				service = inString.substr(pos + 1);
 			}
 			else {
+				// If the colon is not found, the whole string is treated as the host
 				host = inString;
 				// use default port...
 				service = "0";
 			}
-			
+			// Initialize the addrinfo structure
 			addrinfo hint;
-			// Initialize the hint structure to 0
+			// Zero out the hint structure
 			memset(&hint, 0, sizeof(hint));
 			// Set the hint structure to IPv4
 			hint.ai_family = AF_INET;
 			
+			// Result address information
 			addrinfo* result = nullptr;
+			// Call the getaddrinfo function to get the address information from the host and service strings
 			int error = getaddrinfo(host.c_str(), service.c_str(), &hint, &result);
 			// If there is an error and the result is not null
 			if (error != 0 && result != nullptr) {
-				// we release the memory once we copied the desired sockaddr out of the linked list
+				// Free the memory once the desired sockaddr has been copied out of the linked list
 				freeaddrinfo(result);
 				return nullptr;
 			}
-			// if the address is null from the result we go for the next
-			// in the linked list of addresses
+			
+			// If the result address is still null, free the memory and return nullptr
 			while (!result->ai_addr && result->ai_next) {
 				result = result->ai_next; 
 			}
 			// If the address is null we release the memory and return nullptr
 			if (!result->ai_addr) {
+				// Free the memory used by the result
 				freeaddrinfo(result);
 				return nullptr;
 			}
@@ -52,6 +64,7 @@ namespace BloodSockets{
 			auto toRet = std::make_shared<SocketAddress>(*result->ai_addr);
 			// Release the info in result as we have it in the shared ptr
 			freeaddrinfo(result);
+			// Return the created SocketAddress object as a shared pointer.
 			return toRet;
 		}
 	};
